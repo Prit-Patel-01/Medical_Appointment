b2fac960ae3754f31fa5f6cacd7859901a884694
Prit-Patel-01 <khanparaprit3@gmail.com>
2026-03-13 22:15:22 +0530

    #

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Medical_Appointment.Controllers
+namespace Doctor_Appointment_System.Controllers
 {
-    public class AdminController : Controller
+    public class AuthController : Controller
     {
-        public IActionResult Index()
+        // LOGIN PAGE
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        // LOGIN POST
+        [HttpPost]
+        public IActionResult Login(string email, string password, string role)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.Error = "Email and Password required.";
+                return View();
+            }
+
+            switch (role)
+            {
+                case "Patient":
+                    return RedirectToAction("Dashboard", "Patient");
+
+                case "Doctor":
+                    return RedirectToAction("Dashboard", "Doctor");
+
+                case "Admin":
+                    return RedirectToAction("Dashboard", "Admin");
+
+                default:
+                    ViewBag.Error = "Invalid role selected.";
+                    return View();
+            }
+        }
+
+        // REGISTER ROLE SELECT PAGE
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // FORGOT PASSWORD
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string email)
+        {
+            return RedirectToAction("ResetPassword");
+        }
+
+        // RESET PASSWORD
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(string password)
+        {
+            return RedirectToAction("Login");
         }
     }
 }
